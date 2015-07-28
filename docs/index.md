fee5c898272b85963de3c9e8a6806a42366f5ba0
Paweł Maciejewski <pwlmaciejewski@gmail.com>
2015-07-28 09:46:28 -0400

    Add docs

---
+++ docs/index.md
@@ -0,0 +1,94 @@
+# Global
+
+
+
+
+
+* * *
+
+## Class: ListZipper
+Can be used without `new` as well
+
+### ListZipper.toArray() 
+
+Get original array
+
+
+**Example**:
+```js
+ListZipper([1, 2, 3]).toArray() // [1, 2, 3]
+```
+
+### ListZipper.value() 
+
+Get currently focused value
+
+
+**Example**:
+```js
+ListZipper([1, 2, 3], 1).value() // 2
+```
+
+### ListZipper.val() 
+
+Aliast for .value()
+
+
+### ListZipper.index() 
+
+Get currently focused value index in the original array
+
+
+**Example**:
+```js
+ListZipper([1, 2, 3], 1).index() // 1
+```
+
+### ListZipper.i() 
+
+Aliast for .index()
+
+
+### ListZipper.goRight() 
+
+Move current focus to the next element
+
+
+**Example**:
+```js
+ListZipper([1, 2, 3]).goRight().val() // 2
+```
+
+### ListZipper.next() 
+
+Aliast for .goRight()
+
+
+### ListZipper.goLeft() 
+
+Move current focus to the previous element
+
+
+**Example**:
+```js
+ListZipper([1, 2, 3], 1).goLeft().val() // 1
+```
+
+### ListZipper.prev() 
+
+Aliast for .goLeft()
+
+
+
+
+* * *
+
+
+
+**Author:** Paweł Maciejewski &lt;fragphace@gmail.com&gt;
+
+
+
+**Overview:** Immutable list zipper, inspired by Haskell Data.List.Zipper
+
+
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/**
+ * List-Zipper
+ * @overview Immutable list zipper, inspired by Haskell Data.List.Zipper
+ * @author Paweł Maciejewski <fragphace@gmail.com>
+ */
+
+/**
+  * Constructor
+  * @description Can be used without `new` as well.
+  * @constructor
+  * @param {Array} arr - Input list
+  * @param {number} focusedIndex - (optional) Initial value
+  */
 function ListZipper(arr, focusedIndex) {
   if (!(this instanceof ListZipper)) return new ListZipper(arr, focusedIndex);
   focusedIndex = focusedIndex || 0;
@@ -5,26 +18,54 @@ function ListZipper(arr, focusedIndex) {
   this.right = arr.slice(focusedIndex);
 }
 
+/**
+  * toArray
+  * @description Get original array.
+  * @example ListZipper([1, 2, 3]).toArray() // [1, 2, 3]
+  */
 ListZipper.prototype.toArray = function() {
   return this.left.concat(this.right);
 };
 
+/**
+  * value
+  * @description Get currently focused value.
+  * @example ListZipper([1, 2, 3], 1).value() // 2
+  */
 ListZipper.prototype.value = function() {
   return this.right[0];
 };
 
+/**
+  * val
+  * @description Aliast for .value().
+  */
 ListZipper.prototype.val = function() {
   return this.value.apply(this, arguments);
 };
 
+/**
+  * index
+  * @description Get currently focused value index in the original array.
+  * @example ListZipper([1, 2, 3], 1).index() // 1
+  */
 ListZipper.prototype.index = function() {
   return this.left.length;
 };
 
+/**
+  * i
+  * @description Aliast for .index().
+  */
 ListZipper.prototype.i = function() {
   return this.index.apply(this, arguments);
 };
 
+/**
+  * goRight
+  * @description Move current focus to the next element.
+  * @example ListZipper([1, 2, 3]).goRight().val() // 2
+  */
 ListZipper.prototype.goRight = function() {
   return this.mutate(function() {
     if (this.right.length === 1) return;
@@ -32,10 +73,19 @@ ListZipper.prototype.goRight = function() {
   });
 };
 
+/**
+  * next
+  * @description Aliast for .goRight().
+  */
 ListZipper.prototype.next = function() {
   return this.goRight.apply(this, arguments);
 };
 
+/**
+  * goLeft
+  * @description Move current focus to the previous element.
+  * @example ListZipper([1, 2, 3], 1).goLeft().val() // 1
+  */
 ListZipper.prototype.goLeft = function() {
   return this.mutate(function() {
     if (!this.left.length) return;
@@ -43,6 +93,10 @@ ListZipper.prototype.goLeft = function() {
   });
 };
 
+/**
+  * prev
+  * @description Aliast for .goLeft().
+  */
 ListZipper.prototype.prev = function() {
   return this.goLeft.apply(this, arguments);
 };
